89836e07550cdc3378d54f6ef86b81ebcf07dfbf
AndreyIndras <145608538+andreyindras@users.noreply.github.com>
2025-06-21 00:44:40 -0300

    Delete trabalho-final-spring-afk-main directory

--- trabalho-final-spring-afk-main/src/main/java/br/edu/unicesumar/api/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.unicesumar.api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
